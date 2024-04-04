c276d569302028e1f6823e77909b25ec89cc3ca0
mattarwael <155578355+mattarwael@users.noreply.github.com>
2024-04-04 15:16:35 +0300

    first commit

--- intellicampus/intellicampus/api.py
+++ intellicampus/intellicampus/api.py
@@ -1,5 +1,7 @@
 import frappe
 from frappe.utils import get_url, now_datetime
+from frappe import _
+
 
 @frappe.whitelist(allow_guest=True)
 def create_user(student_email, first_name, last_name):
@@ -38,7 +40,7 @@ def send_user_credentials_email(email, first_name, last_name, password):
     subject = "Your Account Credentials"
     message = f"Hello {first_name} {last_name},\n\nYour account has been created.\n\nEmail: {email}\nPassword: {password}\n\nPlease login using these credentials and change your password.\n\nThank you."
 
-    print("Sending email to:", email)  # Debugging line
+    print("Sending email to:", email)
 
     try:
         frappe.sendmail(
@@ -57,21 +59,6 @@ def get_courses():
     courses = frappe.get_all('Course Offered', fields=['course', 'section', 'days', 'course_time',
     'semester', 'instructor', "capacity"])
     return courses
-
-# @frappe.whitelist()
-# def get_courses_details(course_names):
-#     # Split the course names received as a comma-separated string
-#     course_names_list = course_names.split(',')
-
-#     # Fetch details for courses offered with matching course names
-#     courses_details = []
-#     for course_name in course_names_list:
-#         course_details = frappe.get_all("Course", filters={"course_title": course_name}, fields=["course_title", "description", "course_prerequisites", "syllabus", "language", "department", "credits_number"])
-#         if course_details:
-#             courses_details.append(course_details[0])  # Append details for matching courses
-    
-#     return courses_details
-
 
 @frappe.whitelist()
 def get_logged_in_student():
@@ -94,7 +81,7 @@ def get_logged_in_student():
 
 @frappe.whitelist()
 def get_department_courses():
-    #
+    
     user_email = frappe.session.user
     #user_email="wael.m@gmail.com"
     department = frappe.get_value("Student", {"student_email": user_email}, "department")
@@ -113,7 +100,7 @@ def get_department_courses():
 
 @frappe.whitelist()
 def get_logged_in_instructor():
-    #user_email = "samir@gmail.com"  # Replace with actual email or fetch from session
+    #user_email = "samir@gmail.com"  
     user_email = frappe.session.user
 
     instructor = frappe.get_all("Instructor",
@@ -127,3 +114,101 @@ def get_logged_in_instructor():
         return instructor[0]
     else:
         frappe.throw(_("Logged-in user is not an instructor or instructor details not found."), frappe.PermissionError)
+
+
+@frappe.whitelist()
+def get_logged_in_admin():
+    
+    user_email = frappe.session.user
+    #user_email="admin@admin.com"
+    admin_user = frappe.get_all("User",
+            filters={"email": user_email},
+            fields=["name", "first_name","middle_name", "last_name", "email"]
+        )
+
+    if admin_user:
+        return admin_user[0]
+    else:
+        frappe.throw(_("Logged-in user not found."), frappe.PermissionError)
+
+@frappe.whitelist()
+def get_courses_for_instructor():
+    instructor_email = frappe.session.user
+    #instructor_email="samir@gmail.com"
+    # Fetch the instructor's name using their email
+    instructor_name = frappe.get_value('Instructor', {'instructor_email': instructor_email}, 'name')
+    if not instructor_name:
+        frappe.throw(_("Instructor not found with the provided email."), frappe.DoesNotExistError)
+
+    # Fetch courses offered by the instructor, including students_enrolled data
+    courses = frappe.get_all('Course Offered', 
+                             filters={'instructor': instructor_name}, 
+                             fields=['name', 'course', 'section', 'days', 'course_time',
+                                     'semester', 'instructor', 'capacity'])
+
+    # Fetch students enrolled for each course
+    for course in courses:
+        students_enrolled = frappe.get_all('Students Enrolled',
+                                           filters={'parenttype': 'Course Offered', 'parentfield': 'students_enrolled', 'parent': course['name']},
+                                           fields=['student', 'student_grade', 'passfailed'])
+        course['students_enrolled'] = students_enrolled
+
+    return courses
+
+
+@frappe.whitelist()
+def register_student_to_course(course_name, student_name):
+    try:
+        course = frappe.get_doc("Course Offered", course_name)
+        if not course:
+            return _("Course not found")
+
+        student = frappe.get_doc("Student", student_name)
+        if not student:
+            return _("Student not found")
+
+        course.append("students_enrolled", {
+            "student": student_name
+        })
+
+        course.save()
+
+        return _("Student registered to course successfully")
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), _("API Error"))
+        return _("Failed to register student to course: {0}").format(str(e))
+
+@frappe.whitelist()
+def get_student_name(email):
+    student_name = frappe.db.get_value("Student", {"student_email": email}, "name")
+    return student_name
+
+
+@frappe.whitelist()
+def get_courses_for_student(email):
+    student_name = frappe.db.get_value("Student", {"student_email": email}, "name")
+
+    if not student_name:
+        return []
+
+    courses = frappe.db.sql("""
+        SELECT
+            course.name as course_name,
+            course.course,
+            course.section,
+            course.instructor,
+            course.course_time,
+            course.days
+        FROM
+            `tabCourse Offered` AS course
+        JOIN
+            `tabStudents Enrolled` AS enrolled ON course.name = enrolled.parent
+        WHERE
+            enrolled.student = %s
+            AND enrolled.parenttype = 'Course Offered'
+        """, (student_name), as_dict=True)
+
+    return courses
+
+
+
